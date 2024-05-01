6a9c75f0480a8b8861fdfdb1bf34af620cd0fb13
riaan-attar <attarriaan6226@gmail.com>
2024-05-02 00:54:26 +0530

    DB STARTED WORKING

--- ats/models.py
+++ ats/models.py
@@ -8,11 +8,12 @@ class Applicant(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=10)
-    keywords = models.ManyToManyField(keyword, blank=True)
-    exp = models.DecimalField(max_digits = 4,decimal_places = 2, blank=True)
-    cgpa = models.DecimalField(max_digits= 5,decimal_places=2, null=True)
+    # keywords = models.ManyToManyField(keyword, blank=True)
+    keywords = models.TextField(blank=True)
+    # exp = models.DecimalField(max_digits = 4,decimal_places = 2, blank=True)
+    # cgpa = models.DecimalField(max_digits= 5,decimal_places=2, null=True)
     resume = models.FileField(upload_to='resumes/', null=True,blank=True)
-    jd=models.IntegerField(null=True)
+    # jd=models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
